5a631bb6d5f3ff550676eab6abc04fdc4dc5569a
Leo Mancini <mancini@leo.gd>
2022-05-27 20:10:00 -0400

    Updating code formatting

--- history/get.php
+++ history/get.php
@@ -1,8 +1,7 @@
 <?php
-	function getHistorialPrediction($date) {
-		global $SECRETS;
+    function getHistorialPrediction($date) {
+	global $SECRETS;
         $urlBase = $SECRETS['PUBLISH_SERVER_URL'].'history/predictions/';
-
 
         $request = curl_init();
         curl_setopt($request, CURLOPT_URL, $SECRETS['PUBLISH_SERVER_URL'].'history/predictions/'.$date->format('Y-m-d').'.json');
@@ -11,5 +10,5 @@
         curl_close($request);
 
         return json_decode($results);
-	}
+    }
 ?>
